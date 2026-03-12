62bc07bd65cf482160fb8213e430f384fefc50a2
Phan Gia Dat <phangiadat300106@gmail.com>
2026-03-12 09:06:51 +0700

    Cập nhật middlewares phân quyền

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,13 +1,13 @@
-import { NextFunction, Request, Response } from 'express'
-import jwt from 'jsonwebtoken'
+import { NextFunction, Request, Response } from 'express';
+import jwt from 'jsonwebtoken';
 
 export interface AuthRequest extends Request {
-  user?: any
+  user?: any;
 }
 
 export const authenticate = async (req: AuthRequest, res: Response, next: NextFunction) => {
   const authHeader = req.headers.authorization;
-  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
     return res.status(401).json({ message: 'Không tìm thấy Access Token. Vui lòng đăng nhập.' });
   }
 
@@ -20,4 +20,20 @@ export const authenticate = async (req: AuthRequest, res: Response, next: NextFu
   } catch (error) {
     return res.status(401).json({ message: 'Access Token đã hết hạn hoặc không hợp lệ.' });
   }
-}
+};
+
+export const authorizeAdmin = (req: AuthRequest, res: Response, next: NextFunction) => {
+  if (!req.user) {
+    return res.status(401).json({
+      message: 'Vui lòng đăng nhập!',
+    });
+  }
+
+  if (req.user.role !== 'ADMIN') {
+    return res.status(403).json({
+      message: 'Bạn không có quyền thực hiện hành động này!',
+    });
+  }
+
+  next();
+};
