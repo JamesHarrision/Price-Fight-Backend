cb3385ee19578c63dcc0ee63b697d903519190a9
Phan Gia Dat <phangiadat300106@gmail.com>
2026-03-18 17:55:18 +0700

    Final 8

--- src/utils/mailer.util.ts
+++ src/utils/mailer.util.ts
@@ -4,9 +4,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-  host: process.env.EMAIL_HOST,
-  port: Number(process.env.EMAIL_PORT),
-  secure: false, // Use true for port 465, false for port 587
+  service: 'gmail',
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
