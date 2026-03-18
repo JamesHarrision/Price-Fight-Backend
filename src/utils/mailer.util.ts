da703d7ee71465a64bd6befb3049d22edd0077d4
Phan Gia Dat <phangiadat300106@gmail.com>
2026-03-18 18:04:37 +0700

    Final 9

--- src/utils/mailer.util.ts
+++ src/utils/mailer.util.ts
@@ -5,9 +5,14 @@ dotenv.config();
 
 const transporter = nodemailer.createTransport({
   service: 'gmail',
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
+  },
+  tls: {
+    rejectUnauthorized: false,
   },
 });
 
